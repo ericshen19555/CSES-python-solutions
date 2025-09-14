1bc1f461be8e8732daba1d4b5b0050d38efbfa6b
ericshen19555 <ericshen19555@gmail.com>
2025-09-14 23:50:49 +0800

    add: Fixed-Length Paths II - some bottom-up merging magic - O(n) AC

--- Fixed-Length Paths II.py
+++ Fixed-Length Paths II.py
@@ -2,116 +2,46 @@ def main():
     from sys import stdin
     e = stdin.readline
 
-    def add(i, v):
-        i += 1
-        while i <= hi:
-            bit[i] += v
-            i += i & -i
-
-    def que(s, t):
-        s, t = max(0, s), t + 1
-        res = 0
-        while t > s:
-            res += bit[t]
-            t &= t-1
-        while s > t:
-            res -= bit[s]
-            s &= s-1
-        return res
-
     n, lo, hi = map(int, e().split())
-    G = [[] for _ in range(n)]
+    pa = [0] * n
+    deg = [0] * n
+    deg[0] = 2
     for _ in range(n - 1):
         a, b = map(int, e().split())
         a, b = a-1, b-1
-        G[a].append(b)
-        G[b].append(a)
+        deg[a] += 1
+        deg[b] += 1
+        pa[a] ^= b
+        pa[b] ^= a
 
-    done = [False] * n
-    pa = [-1] * n
-    siz = [0] * n
-    bit = [0] * (hi + 1)  # bit
-    dep = [0] * n
+    l = [[1] for _ in range(n)]
+
+    def get(suf, i):
+        if i >= len(suf):
+            return 0
+        if i < 0:
+            return suf[-1]
+        return suf[~i]
 
     ans = 0
-
-    # centroid decomposition
-    cd = [0]
-    for s in cd:
-        # count tree size
-        q = [s]
-        pa[s] = -1
-        for i in q:
+    for i in range(n):
+        while deg[i] == 1:
+            deg[i] = 0
+            y = l[i]
+            y.append(y[-1])
             p = pa[i]
-            siz[i] = 1
-            dep[i] = hi
-            for j in G[i]:
-                if j == p or done[j]: continue
-                pa[j] = i
-                q.append(j)
-        for i in reversed(q):
-            if i == s: continue
-            siz[pa[i]] += siz[i]
-
-        # find centroid
-        half = siz[s] >> 1
-        i = s
-        while True:
-            p = pa[i]
-            for j in G[i]:
-                if j == p or done[j]: continue
-                if siz[j] > half:
-                    i = j
-                    break
-            else:
-                c = i
-                break
-
-        add(0, 1)
-
-        # update ans
-        for s in G[c]:
-            if done[s]: continue
-
-            # update count
-            stk = [s]
-            dep[s] = 1
-            pa[s] = c
-            while stk:
-                i = stk.pop()
-                p, d = pa[i], dep[i]
-                ans += que(lo - d, hi - d)
-                if d == hi: continue
-                nd = d + 1
-                for j in G[i]:
-                    if j == p or done[j]: continue
-                    pa[j], dep[j] = i, nd
-                    stk.append(j)
-
-            # update ans
-            stk = [s]
-            while stk:
-                i = stk.pop()
-                p, d = pa[i], dep[i]
-                add(d, 1)
-                if d == hi: continue
-                for j in G[i]:
-                    if j == p or done[j]: continue
-                    stk.append(j)
-
-        # reset count
-        for i in range(1, hi + 1):
-            v = bit[i]
-            if not v: break
-            while i <= hi:
-                bit[i] -= v
-                i += i & -i
-
-        # recursion
-        done[c] = True
-        for j in G[c]:
-            if not done[j]:
-                cd.append(j)
-
+            x = l[p]
+            # merge x y -> x
+            if len(x) < len(y): x, y = y, x
+            xs, ys = len(x), len(y)
+            for idx in range(ys):
+                ans += ((get(x, lo - idx) - get(x, hi - idx + 1))
+                        * (get(y, idx) - get(y, idx + 1)))
+            for idx in range(ys):
+                x[~idx] += y[~idx]
+            l[p] = x
+            pa[p] ^= i
+            deg[p] -= 1
+            i = p
     print(ans)
 main()
