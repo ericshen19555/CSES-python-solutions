2c1be75e13e1a0f4bc48f325dc0dbe3d9e59f347
ericshen19555 <ericshen19555@gmail.com>
2025-10-04 13:16:41 +0800

    add: Sum of Four Squares - a little bit of math - AC top

--- Sum of Four Squares.py
+++ Sum of Four Squares.py
@@ -1,25 +1,29 @@
 def main():
     from sys import stdin
     e = stdin.readline
-    lim = 10**7 + 9
-
-    def unpack(x):
-        return x >> 12, x & 0xfff
-
-    sqr2 = [-1] * lim
-    for a in range(lim):
-        if a * a >= lim: break
-        aa = a*a
-        a_mask = a << 12
-        for b in range(a + 1):
-            s = aa + b*b
-            if s >= lim: break
-            sqr2[s] = a_mask | b
 
     def solve(n):
-        for x in range(n + 1):
-            if ~sqr2[x] and ~sqr2[n - x]:
-                return *unpack(sqr2[x]), *unpack(sqr2[n - x])
+        if n & 3 == 0:
+            if n == 0:
+                return (0, 0, 0, 0)
+            return tuple(map(lambda x: x << 1, solve(n >> 2)))
+        s = int(n ** 0.5)
+        if n & 7 in (1, 5):
+            a = s - 1 if s & 1 else s
+        else:
+            a = s if s & 1 else s - 1
+        aa = a * a
+        bn = n - aa
+        for b in range(a + 1):
+            bb = b * b
+            if bb > bn: break
+            cn = bn - bb
+            for c in range(b + 1):
+                cc = c * c
+                if cc > cn: break
+                d = int((cn - cc) ** 0.5)
+                if cc + d*d == cn:
+                    return a, b, c, d
 
     ans = []
     for _ in range(int(e())):
