73809bdebded63006a09d40720c7cbaeea1d5770
ericshen19555 <ericshen19555@gmail.com>
2025-10-07 22:49:16 +0800

    add: Knuth Division - O(n²) fast AC

--- Knuth Division.py
+++ Knuth Division.py
@@ -1,24 +1,20 @@
 def main():
     from sys import stdin
-    from itertools import accumulate
     inf = float("INF")
     e = stdin.readline
 
     n = int(e())
-    l = list(accumulate(map(int, e().split()), initial=0))
+    l = [inf] + list(map(int, e().split())) + [inf]
+    ans = 0
 
-    dp = [[0] * i for i in range(n + 1)]
-    best = [[i - 1] * i for i in range(n + 1)]
-    for r in range(2, n + 1):
-        for i in range(n - r + 1):
-            j = i + r
-            dp[j][i] = inf
-            for k in range(best[j - 1][i], best[j][i + 1] + 1):
-                if k == i: continue
-                x = dp[k][i] + dp[j][k]
-                if x < dp[j][i]:
-                    dp[j][i] = x
-                    best[j][i] = k
-            dp[j][i] += l[j] - l[i]
-    print(dp[n][0])
+    for _ in range(n - 1):
+        i = 1
+        while l[i] > l[i + 2]:
+            i += 1
+        l[i] += l.pop(i + 1)
+        ans += l[i]
+        while l[i] > l[i - 1]:
+            l[i], l[i - 1] = l[i - 1], l[i]
+            i -= 1
+    print(ans)
 main()
