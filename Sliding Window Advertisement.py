4f56fd0da62692305495415159311d315a80adad
ericshen19555 <ericshen19555@gmail.com>
2025-10-10 01:24:04 +0800

    add: Sliding Window Advertisement - Iterative LiChao Tree - AC

--- Sliding Window Advertisement.py
+++ Sliding Window Advertisement.py
@@ -11,24 +11,31 @@ def main():
     def y(x, v):
         return x * v[0] + v[1]
 
-    def update(o, s, t, v):
-        mid = s + t >> 1
-        if y(mid, v) > y(mid, tr[o]):
-            tr[o], v = v, tr[o]
-        if y(s, v) > y(s, tr[o]):
-            update(o << 1 | 0, s, mid, v)
-        if y(t - 1, v) > y(t - 1, tr[o]):
-            update(o << 1 | 1, mid, t, v)
-
-    def modify(o, s, t, qs, qt, v):
-        if qt <= s or t <= qs:
-            return
-        if qs <= s and t <= qt:
-            update(o, s, t, v)
-            return
-        mid = s + t >> 1
-        modify(o << 1 | 0, s, mid, qs, qt, v)
-        modify(o << 1 | 1, mid, t, qs, qt, v)
+    def modify(qs, qt, v):
+        stk = [(1, 0, m)]
+        while stk:
+            o, s, t = stk.pop()
+            if qt <= s or  t <= qs: continue
+            if qs <= s and t <= qt:
+                vv = v
+                while True:
+                    mid = s + t >> 1
+                    if y(mid, vv) > y(mid, tr[o]): tr[o], vv = vv, tr[o]
+                    if y(s, vv) > y(s, tr[o]):
+                        o = o << 1 | 0
+                        t = mid
+                        continue
+                    if y(t - 1, vv) > y(t - 1, tr[o]):
+                        o = o << 1 | 1
+                        s = mid
+                        continue
+                    break
+            else:
+                mid = s + t >> 1
+                if qs <= mid:
+                    stk.append((o << 1 | 0, s, mid))
+                if qt > mid:
+                    stk.append((o << 1 | 1, mid, t))
 
     def query(x):
         res = 0
@@ -38,7 +45,7 @@ def main():
             if s + 1 == t: break
             mid = s + t >> 1
             if x < mid:
-                o = o << 1 | 0
+                o = o << 1
                 t = mid
             else:
                 o = o << 1 | 1
@@ -51,11 +58,11 @@ def main():
         if ss > s: s, ss = ss, s
         # h * (x - ss) + h * w
         # h * x + h * (w - ss)
-        modify(1, 0, m, 0, ss, (h, h * (w - ss)))
-        modify(1, 0, m, ss, s, (0, h * w))
+        modify(0, ss, (h, h * (w - ss)))
+        modify(ss, s, (0, h * w))
         # -h * (x - s) + h * w
         # -h * x + h * (w + s)
-        modify(1, 0, m, s, m, (-h, h * (w + s)))
+        modify(s, m, (-h, h * (w + s)))
 
     stk = []
     pi = -1
@@ -70,5 +77,5 @@ def main():
         h = l[pi]
         le = (pi := stk.pop()) + 1
         add(le, n, h)
-    print(*map(query, range(m)))
+    print(*[query(x) for x in range(m)])
 main()
