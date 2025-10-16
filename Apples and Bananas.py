ade92b5caa38089c1c390067720ab4fa785988b0
ericshen19555 <ericshen19555@gmail.com>
2025-10-16 23:31:56 +0800

    try: Apples and Bananas - NTT O(n log n) TLE (even slower than FFT)

--- Apples and Bananas.py
+++ Apples and Bananas.py
@@ -1,30 +1,34 @@
+mod = 9223372036737335297
+mod = 75161927681
+g = 3
+
 def main():
     from sys import stdin
-    import cmath
     e = stdin.readline
 
-    def fft(l, inv):
+    def ntt(l, inv):
         n = len(l)
         for i in range(n):
             if i < (j := rev_bit[i]):
                 l[i], l[j] = l[j], l[i]
         b = 1
         while b < n:
-            wn = cmath.exp(inv * 1j * cmath.pi / b)
+            wn = pow(g, inv * (mod - 1) // (b * 2), mod)
             for i in range(0, n, b * 2):
                 w = 1
                 for j in range(i, i + b):
                     x, y = l[j + 0], l[j + b] * w
-                    l[j + 0] = x + y
-                    l[j + b] = x - y
-                    w *= wn
+                    l[j + 0] = (x + y) % mod
+                    l[j + b] = (x - y) % mod
+                    w = (w * wn) % mod
             b <<= 1
         if inv == -1:
             for i in range(n):
-                l[i] /= n
+                l[i] = (l[i] * inv_n) % mod
 
     k, n, m = map(int, e().split())
     lim = 1 << k.bit_length() + 1  # min 2^x > 2k
+    inv_n = pow(lim, -1, mod)
     rev_bit = [0] * lim
     for i in range(1, lim):
         rev_bit[i] = rev_bit[i >> 1] >> 1 | (i & 1) * (lim >> 1)
@@ -35,10 +39,10 @@ def main():
         a[v] += 1
     for v in map(int, e().split()):
         b[v] += 1
-    fft(a, 1)
-    fft(b, 1)
+    ntt(a, 1)
+    ntt(b, 1)
     for i in range(lim):
-        a[i] *= b[i]
-    fft(a, -1)
-    print(*[round(v.real) for v in a[2:2*k+1]])
+        a[i] = (a[i] * b[i]) % mod
+    ntt(a, -1)
+    print(*a[2:2*k+1])
 main()
