fbf9fd55da714e8308d44700b4e9c5c9918acbf2
ericshen19555 <ericshen19555@gmail.com>
2025-10-25 01:35:57 +0800

    add: Creating Offices - O(n) DFS AC

--- Creating Offices.py
+++ Creating Offices.py
@@ -1,87 +1,60 @@
+# credit: ammojamo - https://cses.fi/problemset/hack/1752/entry/9695386/
+
 def main():
     from sys import stdin
     e = stdin.readline
 
     n, k = map(int, e().split())
-    h = n.bit_length()
-    G = [[] for _ in range(n)]
-    for _ in range(n - 1):
+    ne = [-1] * (n << 1)
+    xe = [0] * n
+    hd = [-1] * n
+    for ei in range(n - 1):
         a, b = map(int, e().split())
         a, b = a - 1, b - 1
-        G[a].append(b)
-        G[b].append(a)
-    cd = [n] * n + [-1]
-    cp = [0] * n
-    siz = [0] * (n + 1)
-    pa = [-1] * n
-
-    rec = [0]
-    dis = [[-1] * n for _ in range(h)]
-    for r in rec:
-        cpc = pa[r]
-        d = cd[cpc] + 1
-        q = [r]
-        pa[r] = -1
-        siz[r] = 1
-        for i in q:
-            p = pa[i]
-            for j in G[i]:
-                if j == p or cd[j] < d: continue
-                siz[j] = 1
-                pa[j] = i
-                q.append(j)
-        for i in reversed(q):
-            siz[pa[i]] += siz[i]
-        half = siz[r] >> 1
-        i = r
-        while True:
-            p = pa[i]
-            for j in G[i]:
-                if j == p or cd[j] < d: continue
-                if siz[j] > half:
-                    i = j
-                    break
-            else:
-                c = i
-                break
-        cp[c] = cpc
-        cd[c] = d
-        dis_d = dis[d]
-        dis_d[c] = 0
-        q = [c]
-        for i in q:
-            v = dis_d[i]
-            for j in G[i]:
-                if cd[j] < d or ~dis_d[j]: continue
-                dis_d[j] = v + 1
-                q.append(j)
-        for j in G[c]:
-            if cd[j] < d: continue
-            pa[j] = c
-            rec.append(j)
-
-    q = [0]
-    for i in q:
-        p = pa[i]
-        for j in G[i]:
-            if j == p: continue
-            pa[j] = i
-            q.append(j)
-
-    ans = []
-    mn = [k] * n
-    for i in reversed(q):
-        c, d = i, cd[i]
-        for d in range(cd[i], -1, -1):
-            if dis[d][i] + mn[c] < k:
-                break
-            c = cp[c]
+        xe[ei] = a ^ b
+        ea, eb = ei << 1, ei << 1 | 1
+        if ~hd[a]:
+            ne[hd[a]], ne[ea] = eb, ne[hd[a]]
         else:
-            ans.append(i + 1)
-            c, d = i, cd[i]
-            for d in range(cd[i], -1, -1):
-                mn[c] = min(mn[c], dis[d][i])
-                c = cp[c]
+            ne[ea], hd[a] = eb, ea
+        if ~hd[b]:
+            ne[hd[b]], ne[eb] = ea, ne[hd[b]]
+        else:
+            ne[eb], hd[b] = ea, eb
+    i = d = 0
+    dep = [0] * n + [float("INF")]
+    ei, ne[hd[i]] = ne[hd[i]], -1
+    no = [-1] * n
+    ans = [False] * n
+    while ~ei:
+        j = xe[ei >> 1] ^ i
+        if j >= 0:
+            xe[ei >> 1] = ~xe[ei >> 1]
+            d += 1
+            dep[j] = d
+        else:
+            j = ~j
+            if dep[no[i]] - d >= k:
+                ans[i] = True
+                no[i] = i
+            d -= 1
+            if not no[j]:
+                no[j] = no[i]
+            else:
+                di, dj = dep[no[i]], dep[no[j]]
+                if di - d + dj - d < k:
+                    if di > dj:
+                        ans[no[j]] = False
+                        no[j] = no[i]
+                    else:
+                        ans[no[i]] = False
+                elif di < dj:
+                    no[j] = no[i]
+        i = j
+        ei = ne[ei]
+    if dep[no[i]] - d >= k:
+        ans[i] = True
+    ans = [i + 1 for i, v in enumerate(ans) if v]
     print(len(ans))
     print(*ans)
 main()
