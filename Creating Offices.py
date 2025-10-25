4304eb095995123ca00b831b2997160e607cc492
ericshen19555 <ericshen19555@gmail.com>
2025-10-25 15:01:42 +0800

    add: Creating Offices - Optimized iterative bottom-up DP - O(n) TOP

--- Creating Offices.py
+++ Creating Offices.py
@@ -1,59 +1,47 @@
-# credit: ammojamo - https://cses.fi/problemset/hack/1752/entry/9695386/
-
 def main():
     from sys import stdin
     e = stdin.readline
 
     n, k = map(int, e().split())
-    ne = [-1] * (n << 1)
-    xe = [0] * n
-    hd = [-1] * n
+    pa = [0] * n
+    deg = [0] * n
+    deg[0] = 2
     for ei in range(n - 1):
         a, b = map(int, e().split())
         a, b = a - 1, b - 1
-        xe[ei] = a ^ b
-        ea, eb = ei << 1, ei << 1 | 1
-        if ~hd[a]:
-            ne[hd[a]], ne[ea] = eb, ne[hd[a]]
-        else:
-            ne[ea], hd[a] = eb, ea
-        if ~hd[b]:
-            ne[hd[b]], ne[eb] = ea, ne[hd[b]]
-        else:
-            ne[eb], hd[b] = ea, eb
-    i = d = 0
-    dep = [0] * n + [float("INF")]
-    ei, ne[hd[i]] = ne[hd[i]], -1
-    no = [-1] * n
+        pa[a] ^= b
+        pa[b] ^= a
+        deg[a] += 1
+        deg[b] += 1
     ans = [False] * n
-    while ~ei:
-        j = xe[ei >> 1] ^ i
-        if j >= 0:
-            xe[ei >> 1] = ~xe[ei >> 1]
-            d += 1
-            dep[j] = d
-        else:
-            j = ~j
-            if dep[no[i]] - d >= k:
+    no = [(-1, k)] * n
+    for i in range(n):
+        while deg[i] == 1:
+            deg[i] = 0
+            x, dx = no[i]
+            if dx >= k:
                 ans[i] = True
-                no[i] = i
-            d -= 1
-            if not no[j]:
-                no[j] = no[i]
+                x, dx = i, 1
             else:
-                di, dj = dep[no[i]], dep[no[j]]
-                if di - d + dj - d < k:
-                    if di > dj:
-                        ans[no[j]] = False
-                        no[j] = no[i]
-                    else:
-                        ans[no[i]] = False
-                elif di < dj:
-                    no[j] = no[i]
-        i = j
-        ei = ne[ei]
-    if dep[no[i]] - d >= k:
-        ans[i] = True
+                dx += 1
+
+            p = pa[i]
+            y, dy = no[p]
+            if y == -1:
+                no[p] = x, dx
+            elif dx + dy >= k:
+                if dx < dy:
+                    no[p] = x, dx
+            elif dx > dy:
+                ans[y] = False
+                no[p] = x, dx
+            else:
+                ans[x] = False
+            pa[p] ^= i
+            deg[p] -= 1
+            i = p
+    if no[0][1] >= k:
+        ans[0] = True
     ans = [i + 1 for i, v in enumerate(ans) if v]
     print(len(ans))
     print(*ans)
