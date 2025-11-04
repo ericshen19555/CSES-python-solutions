7ed769a9bde844ed64d70973b0179c061ae8b3af
ericshen19555 <ericshen19555@gmail.com>
2025-11-04 19:19:23 +0800

    add: Tree Coin Collecting II - O(n log n) AC (top time C++)

--- Tree Coin Collecting II.py
+++ Tree Coin Collecting II.py
@@ -1,61 +1,45 @@
-import sys
-from io import StringIO
-
-testcase = """\
-4 1
-0 0 1 1
-1 2
-2 3
-1 4
-3 4
-"""
-
-sys.stdin = StringIO(testcase)
-
 def main():
     from sys import stdin
     e = stdin.readline
 
     n, q = map(int, e().split())
     l = list(map(int, e().split()))
+    r = l.index(1)  # 以 coin 為根，則子樹中有 coin 即為重要點 (題目保證至少有 1 coin)
     G = [[] for _ in range(n)]
+    pa = [0] * n
+    deg = [0] * n
+    deg[r] = 2
     for _ in range(n - 1):
         a, b = map(int, e().split())
         a, b = a - 1, b - 1
         G[a].append(b)
         G[b].append(a)
+        pa[a] ^= b
+        pa[b] ^= a
+        deg[a] += 1
+        deg[b] += 1
 
-    # v = l[i]: v & 1 表示自己是否為重要點, v >> 1 表示 i 在無根樹上有多少子樹有 coin
-    qu = [0]  # 第一輪 dfs 序，用來建 top
-    stk = [0]
-    idx = [0] * n  # 遍歷 G[i]
-    pa = [-1] * n
+    qu = [0] * n
+    x = n
     ch = [-1] * n
     siz = [1] * n
-    dep = [0] * n
-    while stk:
-        i = stk[-1]
-        p = pa[i]
-        it, x = G[i], idx[i]
-        if x < len(it):
-            j = it[x]
-            idx[i] += 1
-            if j == p: continue
-            qu.append(j)
-            pa[j] = i
-            dep[j] = dep[i] + 1
-            if l[i]: l[j] |= 0b10  # 如果 i 或 i 的祖先有 coin，則更新給 j
-            stk.append(j)
-        else:
-            if l[i] >> 2: l[i] = 1  # 如果無根樹上 >= 2 個子樹有 coin，自己也是重要點
-            if ~p:
-                l[p] += (l[i] & 1) << 1  # 如果 i 是重要點，則更新給 p
-                siz[p] += siz[i]
-                if ch[p] == -1 or siz[i] > siz[ch[p]]:
-                    ch[p] = i
-            stk.pop()
+    for i in range(n):
+        while deg[i] == 1:
+            deg[i] = 0
+            x -= 1
+            qu[x] = i
+            p = pa[i]
+            l[p] |= l[i]  # 子樹內有 coin 則為重要點
+            if ch[p] == -1 or siz[ch[p]] < siz[i]:
+                ch[p] = i
+            siz[p] += siz[i]
+            pa[p] ^= i
+            deg[p] -= 1
+            i = p
+    qu[0] = r
+
     # l[i]: i 離重要點的最近距離
-    l = [0 if v & 1 else n for v in l]
+    l = [0 if v else n for v in l]
     bfs = [i for i, v in enumerate(l) if v == 0]
     base = len(bfs) - 1 << 1  # 遍歷所有重要點並回到原位的距離
     for i in bfs:
@@ -70,12 +54,14 @@ def main():
     mn = [0] * n  # mn[i]: i 所在重鏈上，l 的前綴 min
     zkw = [0] * (n << 1)  # zkw[dfn[i]]: l 的區間 min
     top = [-1] * n
+    dep = [0] * n
     for t in qu:
         if ~top[t]: continue
         i = t
         x = n
         while ~i:
             top[i] = t
+            dep[i] = dep[pa[i]] + 1
             dfn[i] = dfc
             mn[i] = x = min(x, l[i])
             zkw[dfc + n] = l[i]
@@ -83,31 +69,29 @@ def main():
             i = ch[i]
     for i in range(n - 1, 0, -1): zkw[i] = min(zkw[i << 1], zkw[i << 1 | 1])
 
-    def lca(a, b):
-        res = n
+    ans = []
+    for _ in range(q):
+        a, b = map(int, e().split())
+        a, b = a - 1, b - 1
+        # 2*key + 2*dis_to_key[a] + 2*dis_to_key[b] - dis[a][b] - 2*min(dis_to_key[a...b])
+        res = base + (l[a] + l[b]) * 2 - dep[a] - dep[b]
+        v = n
         while top[a] != top[b]:
             if dep[top[a]] < dep[top[b]]: a, b = b, a
-            res = min(res, mn[a])
+            v = min(v, mn[a])
             a = pa[top[a]]
         if dep[a] < dep[b]: a, b = b, a
         s, t = n + dfn[b], n + dfn[a] + 1
         while s < t:
             if s & 1:
-                res = min(res, zkw[s])
+                v = min(v, zkw[s])
                 s += 1
             if t & 1:
                 t -= 1
-                res = min(res, zkw[t])
+                v = min(v, zkw[t])
             s >>= 1
             t >>= 1
-        return dep[b] - res
-
-    ans = []
-    q = 1
-    for _ in range(q):
-        a, b = map(int, e().split())
-        a, b = a - 1, b - 1
-        # 2*key + 2*dis_to_key[a] + 2*dis_to_key[b] - dis[a][b] - 2*min(dis_to_key[a...b])
-        ans.append(base + (l[a] + l[b] + lca(a, b)) * 2 - dep[a] - dep[b])
+        res += (dep[b] - v) * 2
+        ans.append(res)
     print("\n".join(map(str, ans)))
 main()
