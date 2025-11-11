89f1db007d79f64bd383ee90266aab5f044cc963
ericshen19555 <ericshen19555@gmail.com>
2025-11-12 00:22:57 +0800

    add: Empty String - AC

--- Empty String.py
+++ Empty String.py
@@ -1,15 +1,16 @@
-from itertools import accumulate
 mod = 10**9 + 7
 
 def main():
-    mul = lambda a, b: a * b % mod
     s = input()
     n = len(s)
 
-    lim = n + 1
-    fac = list(accumulate(range(1, lim), func=mul, initial=1))
-    inv = list(accumulate(range(n, 0, -1), func=mul, initial=pow(fac[-1], -1, mod)))[::-1]
-    C = lambda n, k: fac[n] * inv[k] * inv[n - k] % mod
+    lim = n // 2 + 1
+    C = [[0] * lim for _ in range(lim)]
+    C[0][0] = 1
+    for i in range(1, lim):
+        C[i][0] = 1
+        for j in range(1, i + 1):
+            C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % mod
 
     dp = [[0] * i + [1] for i in range(1, n + 1)]
     for r in range(1, n):
@@ -18,7 +19,7 @@ def main():
             x = 0
             for k in range(i + 1, j + 1):
                 if s[i] == s[k]:
-                    x += dp[k-1][i+1] * dp[j][k+1] * C(j - i + 1 >> 1, k - i + 1 >> 1) % mod
+                    x += (dp[k-1][i+1] * dp[j][k+1] % mod) * C[j - i + 1 >> 1][k - i + 1 >> 1] % mod
             dp[j][i] = x % mod
     print(dp[-1][0])
 main()
