75c38c9c6e3529f012e188a37c4e04b8d885f96f
ericshen19555 <ericshen19555@gmail.com>
2025-11-19 01:12:29 +0800

    add: Nested Ranges Count - Fast Radix Sort - AC top

--- Nested Ranges Count.py
+++ Nested Ranges Count.py
@@ -1,39 +1,76 @@
 def main():
     from sys import stdin
-    from operator import itemgetter
     e = stdin.readline
 
     n = int(e())
-    l = [(*map(int, e().split()), i) for i in range(n)]
-    l.sort(key=lambda x: (x[0], -x[1]))
-    mp = {v: i for i, v in enumerate(sorted(set(map(itemgetter(1), l))))}
-    m = len(mp)
-    bit = [0] * (n + 1)
+    a = [0] * n
+    b = [0] * n
+    for i in range(n):
+        a[i], b[i] = map(int, e().split())
+
+    siz = 1 << 15
+    l = [0] * n
+    s = [0] * n
+    p = [0] * n
+    c = [0] * siz
+    for i in range(n): c[b[i] & 32767] += 1
+    for i in range(1, siz): c[i] += c[i - 1]
+    for i in range(n - 1, -1, -1):
+        x = b[i] & 32767
+        c[x] -= 1
+        p[c[x]] = i
+    c = [0] * siz
+    for i in range(n): c[b[p[i]] >> 15] += 1
+    for i in range(1, siz): c[i] += c[i - 1]
+    for i in range(n - 1, -1, -1):
+        x = b[p[i]] >> 15
+        c[x] -= 1
+        l[~c[x]] = p[i]
+    pre = 0
+    for i in range(n):
+        cur = b[l[i]]
+        s[l[i]] = s[l[i - 1]] if pre == cur else i
+        pre = cur
+    c = [0] * siz
+    for i in range(n): c[a[l[i]] & 32767] += 1
+    for i in range(1, siz): c[i] += c[i - 1]
+    for i in range(n - 1, -1, -1):
+        x = a[l[i]] & 32767
+        c[x] -= 1
+        p[c[x]] = l[i]
+    c = [0] * siz
+    for i in range(n): c[a[p[i]] >> 15] += 1
+    for i in range(1, siz): c[i] += c[i - 1]
+    for i in range(n - 1, -1, -1):
+        x = a[p[i]] >> 15
+        c[x] -= 1
+        l[c[x]] = p[i]
+
     ans = [0] * n
-    for a, b, i in reversed(l):
-        b = mp[b] + 1
+    bit = [0] * (n + 1)
+    for i in range(n-1, -1, -1):
+        i = l[i]
+        x = v = n - s[i]
         res = 0
-        x = b
         while x:
             res += bit[x]
             x &= x - 1
         ans[i] = res
-        x = b
+        x = v
         while x <= n:
             bit[x] += 1
             x += x & -x
     print(*ans)
     bit = [0] * (n + 1)
-    l.sort(key=lambda x: (x[0], -x[1]))
-    for a, b, i in l:
-        b = m - mp[b]
+    for i in range(n):
+        i = l[i]
+        x = v = s[i] + 1
         res = 0
-        x = b
         while x:
             res += bit[x]
             x &= x - 1
         ans[i] = res
-        x = b
+        x = v
         while x <= n:
             bit[x] += 1
             x += x & -x
