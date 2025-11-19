16cc51a2aa527724623b393a72bb81da4dce5e19
ericshen19555 <ericshen19555@gmail.com>
2025-11-20 01:31:49 +0800

    add: Additional Problems II * 1 - Minimum Cost Pairs - O(n log n) Regret Greedy on Vally-Mono Stack (fast)

--- Minimum Cost Pairs.py
+++ Minimum Cost Pairs.py
@@ -1,37 +1,33 @@
+# credit: dreamoon
+
 def main():
     from sys import stdin
-    from heapq import heapify, heappop, heappush
+    inf = 10**10
     e = stdin.readline
-
-    def encode(v, s, t):
-        return v << 40 | s << 20 | t
-
-    def decode(x):
-        return x >> 40, (x >> 20) & 0xfffff, x & 0xfffff
 
     n = int(e())
     l = sorted(map(int, e().split()))
     for i in range(n - 1):
         l[i] = l[i + 1] - l[i]
+    l[-1] = inf
 
-    ans = [0] * (n >> 1)
-    le = list(range(n))
-    ri = list(range(n))
-    h = [encode(l[i], i, i) for i in range(n)]
-    heapify(h)
+    ds = []
+    stk = [inf]
+    pp, p = inf + 1, inf + 2
+    for v in l:
+        while pp >= p <= v:
+            ds.append(p)
+            v += pp - p
+            p = stk.pop()
+            pp = stk.pop()
+        stk.append(pp)
+        pp, p = p, v
+    ds.sort()
+
     cur = 0
-    for i in range(n >> 1):
-        while True:
-            v, s, t = decode(heappop(h))
-            if not s == le[s] == le[t]: continue
-            if not t == ri[s] == ri[t]: continue
-            break
-        cur += v
-        ans[i] = cur
-        if s == n - 1 or t == n - 1: continue
-        s, t = le[s - 1], ri[t + 1]
-        le[t], ri[s] = s, t
-        l[s] = l[t] = v = l[s] + l[t] - v
-        heappush(h, encode(v, s, t))
-    print(" ".join(map(str, ans)))
+    ans = []
+    for d in ds:
+        cur += d
+        ans.append(cur)
+    print(*ans)
 main()
