e759508a17128370d4f1ed913b6819d287a5e2e1
ericshen19555 <ericshen19555@gmail.com>
2025-11-24 18:17:37 +0800

    add: Additional Problems II * 1 - Grid Coloring II - magic greedy fast AC

--- Grid Coloring II.py
+++ Grid Coloring II.py
@@ -1,65 +1,43 @@
+# credit: qusol
+
 def main():
     from sys import stdin
+    from collections import deque
     e = stdin.readline
 
     m, n = map(int, e().split())
     l = [bytearray(e().encode()) for _ in range(m)]
-    siz = m * n << 1
-    scc = [-1] * siz
-    q = []
+    ans = [bytearray(n) for _ in range(m)]
 
-    encode = lambda i, j, c: i * n + j << 1 | c
-    decode = lambda x: ((x >> 1) // n, (x >> 1) % n, x & 1)
-    colors = [1, 2, 0, 2, 0, 1]
-    color = lambda c, x: colors[c - 65 << 1 | x]
+    d = [0, -1, 0, 1]
+    t = lambda c: 1 << c - 65
+    q = deque()
 
-    def edge(a, b, c, d, e, f):
-        if not (0 <= a < m and 0 <= b < n): return False
-        if not (0 <= d < m and 0 <= e < n): return False
-        return color(l[a][b], c) == color(l[d][e], f ^ 1)
-
-    d = [0, 1, 0, -1]
-    for x in range(siz):
-        if ~scc[x]: continue
-        scc[x] = 0
-        stk = [x << 4]
-        while stk:
-            x = stk.pop()
-            if x & 8:
-                q.append(x >> 4)
+    def f(i, j, bad):
+        q.append((i, j, bad))
+        while q:
+            i, j, bad = q.popleft()
+            if not (0 <= i < m and 0 <= j < n): continue
+            if bad == l[i][j]: continue
+            if ans[i][j]:
+                if bad == ans[i][j]:
+                    print("IMPOSSIBLE")
+                    exit()
                 continue
-            stk.append(x + 1)
-            x, di, nc = x >> 4, (x >> 1) & 3, x & 1
-            i, j, c = decode(x)
-            ni, nj = i + d[di], j + d[di ^ 1]
-            if not edge(i, j, c, ni, nj, nc): continue
-            nx = encode(ni, nj, nc)
-            if ~scc[nx]: continue
-            scc[nx] = 0
-            stk.append(nx << 4)
+            c = ans[i][j] = 65 ^ 66 ^ 67 ^ l[i][j] ^ bad
+            for di in range(4):
+                q.append((i + d[di], j + d[di ^ 1], c))
 
-    scn = 0
-    for x in reversed(q):
-        if scc[x]: continue
-        scn += 1
-        scc[x] = scn
-        stk = [x]
-        while stk:
-            x = stk.pop()
-            i, j, c = decode(x)
-            for nc in range(2):
-                for di in range(4):
-                    ni, nj = i + d[di], j + d[di ^ 1]
-                    if not edge(ni, nj, nc, i, j, c): continue
-                    nx = encode(ni, nj, nc)
-                    if scc[nx]: continue
-                    scc[nx] = scn
-                    stk.append(nx)
-    if any(scc[x] == scc[x | 1] for x in range(0, siz, 2)):
-        print("IMPOSSIBLE")
-    else:
-        for x in range(0, siz, 2):
-            i, j, _ = decode(x)
-            l[i][j] = color(l[i][j], scc[x] < scc[x | 1]) + 65
-        print("".join(map(bytearray.decode, l)))
+    for i in range(m - 1):
+        for j in range(n - 1):
+            if t(l[i][j]) | t(l[i+1][j]) | t(l[i][j+1]) | t(l[i+1][j+1]) != 7: continue
+            if l[i+0][j+0] == l[i+1][j+0]: f(i+0, j+0, l[i+1][j+1])
+            if l[i+0][j+0] == l[i+0][j+1]: f(i+0, j+0, l[i+1][j+1])
+            if l[i+1][j+1] == l[i+1][j+0]: f(i+1, j+1, l[i+0][j+0])
+            if l[i+1][j+1] == l[i+0][j+1]: f(i+1, j+1, l[i+0][j+0])
+    for i in range(m):
+        for j in range(n):
+            if ans[i][j]: continue
+            f(i, j, l[i][j] + 1 if l[i][j] < 67 else 65)
+    print("\n".join(map(bytearray.decode, ans)))
 main()
